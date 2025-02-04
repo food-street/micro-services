2e2d62809e001a086708735c204b63d95ef78f85
Imthath <imthath.m@hotmail.com>
2025-02-04 15:15:55 +0530

    otp service: add tests

--- otp-service/src/test/java/com/imthath/food_street/otp/OtpServiceApplicationTests.java
+++ otp-service/src/test/java/com/imthath/food_street/otp/OtpServiceApplicationTests.java
@@ -1,13 +1,101 @@
 package com.imthath.food_street.otp;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.utility.DockerImageName;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class OtpServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@ServiceConnection
+	static GenericContainer<?> redis = new GenericContainer<>(DockerImageName.parse("redis:6.2.6"))
+			.withExposedPorts(6379);
+
+	@LocalServerPort
+	private int port;
+
+	@BeforeEach
+	void setUp() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
 	}
 
+	@Test
+	void badRequests() {
+		RestAssured
+				.given()
+				.post("/otp/generate")
+				.then()
+				.statusCode(400);
+
+		RestAssured
+				.given()
+				.param("identifier", "test-bad-request")
+				.put("/otp/validate")
+				.then()
+				.statusCode(400);
+	}
+
+	@Test
+	void missingOTP() {
+		RestAssured
+				.given()
+				.param("identifier", "test-missing")
+				.param("otp", "1234")
+				.put("/otp/validate")
+				.then()
+				.statusCode(428);
+	}
+
+	@Test
+	void invalidOTP() {
+		RestAssured
+				.given()
+				.param("identifier", "test-invalid")
+				.post("/otp/generate")
+				.then()
+				.statusCode(201);
+
+		RestAssured
+				.given()
+				.param("identifier", "test-invalid")
+				.param("otp", "123456")
+				.put("/otp/validate")
+				.then()
+				.statusCode(409);
+	}
+
+	@Test
+	void validOTP() {
+		var otp = RestAssured
+				.given()
+				.param("identifier", "test-valid")
+				.post("/otp/generate")
+				.then()
+				.statusCode(201)
+				.extract()
+				.body()
+				.asString();
+
+		RestAssured
+				.given()
+				.param("identifier", "test-valid")
+				.param("otp", otp)
+				.put("/otp/validate")
+				.then()
+				.statusCode(202);
+
+		RestAssured
+				.given()
+				.param("identifier", "test-valid")
+				.param("otp", otp)
+				.put("/otp/validate")
+				.then()
+				.statusCode(428);
+	}
 }
