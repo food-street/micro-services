6eea6bea59853fc8aad8f9a2950d22601d30e0b1
Imthath <imthath.m@hotmail.com>
2025-02-04 07:04:33 +0530

    otp service: fix typos

--- otp-service/src/main/java/com/imthath/food_street/otp/OtpController.java
+++ otp-service/src/main/java/com/imthath/food_street/otp/OtpController.java
@@ -2,23 +2,20 @@ package com.imthath.food_street.otp;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/otp")
 public class OtpController {
     @Autowired private OtpService otpService;
 
-    @RequestMapping("/generate")
+    @PostMapping("/generate")
     @ResponseStatus(HttpStatus.CREATED)
     public String generateOtp(@RequestParam String identifier) {
         return otpService.generateOtp(identifier);
     }
 
-    @RequestMapping("/validate")
+    @PutMapping("/validate")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public boolean validateOtp(@RequestParam String identifier, @RequestParam String otp) {
         return otpService.validateOtp(identifier, otp);
