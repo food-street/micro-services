a6c8cf707826a179e4074c838247ca158a727cff
Imthath <imthath.m@hotmail.com>
2025-02-05 06:57:25 +0530

    message central: fix env variables

--- user-service/src/main/java/com/imthath/food_street/user_service/message_central/TokenService.java
+++ user-service/src/main/java/com/imthath/food_street/user_service/message_central/TokenService.java
@@ -1,13 +1,22 @@
 package com.imthath.food_street.user_service.message_central;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 class TokenService {
-    static final String CUSTOMER_ID = System.getenv("MC_CUSTOMER_ID");
-    static final String KEY = System.getenv("MC_KEY");
-    static final String SCOPE = "NEW";
+    String CUSTOMER_ID;
+    String KEY;
+    String SCOPE = "NEW";
+
+    @PostConstruct
+    void init() {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        CUSTOMER_ID = dotenv.get("MC_CUSTOMER_ID");
+        KEY = dotenv.get("MC_KEY");
+    }
 
     @Autowired
     TokenClient tokenClient;
