87942e9237efe888984ddd52f40b9dabbefd8353
Imthath <imthath.m@hotmail.com>
2025-02-05 14:19:35 +0530

    user service: remove .env loader dependency

--- user-service/src/main/java/com/imthath/food_street/user_service/message_central/TokenService.java
+++ user-service/src/main/java/com/imthath/food_street/user_service/message_central/TokenService.java
@@ -1,22 +1,17 @@
 package com.imthath.food_street.user_service.message_central;
 
-import io.github.cdimascio.dotenv.Dotenv;
-import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 class TokenService {
-    String CUSTOMER_ID;
-    String KEY;
-    String SCOPE = "NEW";
 
-    @PostConstruct
-    void init() {
-        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-        CUSTOMER_ID = dotenv.get("MC_CUSTOMER_ID");
-        KEY = dotenv.get("MC_KEY");
-    }
+    @Value("${MC_CUSTOMER_ID}")
+    private String CUSTOMER_ID;
+
+    @Value("${MC_KEY}")
+    private String KEY;
 
     @Autowired
     TokenClient tokenClient;
@@ -24,11 +19,6 @@ class TokenService {
     static final String HEADER_KEY = "authToken";
 
     String generateAuthToken() {
-        try {
-            return tokenClient.generateAuthToken(CUSTOMER_ID, SCOPE, KEY).token();
-        } catch (Exception e) {
-            System.out.println("Error while generating auth token: " + e.getMessage());
-            return "null";
-        }
+        return tokenClient.generateAuthToken(CUSTOMER_ID, "NEW", KEY).token();
     }
 }
