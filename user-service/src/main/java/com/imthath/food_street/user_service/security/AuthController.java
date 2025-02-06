efa46f29eb29a42b6adc945660b21db6513ea834
Imthath <imthath.m@hotmail.com>
2025-02-06 15:16:09 +0530

    auth controller: udpate status

--- user-service/src/main/java/com/imthath/food_street/user_service/security/AuthController.java
+++ user-service/src/main/java/com/imthath/food_street/user_service/security/AuthController.java
@@ -1,6 +1,7 @@
 package com.imthath.food_street.user_service.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,11 +12,13 @@ public class AuthController {
     AuthService authService;
 
     @PostMapping("/send-otp")
+    @ResponseStatus(HttpStatus.CREATED)
     OtpResponse sendOtp(@RequestParam @ValidPhoneNumber String phone) {
         return authService.sendOtp(phone);
     }
 
     @PutMapping("/validate-otp")
+    @ResponseStatus(HttpStatus.ACCEPTED)
     JwtResponse validateOtp(@RequestParam String otp, @RequestHeader("identifier") String identifier) {
         return authService.validateOtp(otp, identifier);
     }
