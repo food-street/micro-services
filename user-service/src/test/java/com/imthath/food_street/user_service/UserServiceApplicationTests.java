195a3439f9ffe137d12d7a481b82e9cf7bd85579
Imthath <imthath.m@hotmail.com>
2025-02-06 12:43:15 +0530

    user service tests: test invalid phone numbers

--- user-service/src/test/java/com/imthath/food_street/user_service/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/imthath/food_street/user_service/UserServiceApplicationTests.java
@@ -1,15 +1,37 @@
 package com.imthath.food_street.user_service;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class UserServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@LocalServerPort
+	private int port;
+
+	@BeforeEach
+	void setup() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
 	}
 
+	@Test
+	void invalidPhoneNumbers() {
+		RestAssured.given()
+				.param("phone", "123")
+				.post("/auth/v1/send-otp")
+				.then()
+				.statusCode(400);
+
+		RestAssured.given()
+				.param("phone", "testing")
+				.post("/auth/v1/send-otp")
+				.then()
+				.statusCode(400);
+	}
 }
