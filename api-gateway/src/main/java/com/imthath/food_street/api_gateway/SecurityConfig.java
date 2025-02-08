e4b33305404e484c4f2d5481109517210ecf3c53
Imthath <imthath.m@hotmail.com>
2025-02-08 11:26:33 +0530

    gateway: disable security with web customizer

--- api-gateway/src/main/java/com/imthath/food_street/api_gateway/SecurityConfig.java
+++ api-gateway/src/main/java/com/imthath/food_street/api_gateway/SecurityConfig.java
@@ -1,23 +1,31 @@
 package com.imthath.food_street.api_gateway;
 
-import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.stereotype.Controller;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
 
-//@Configuration
-//public class SecurityConfig {
-//
-//    @Bean
-//    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        System.out.println("setting up bean for security config");
-//        return http
-//                .authorizeHttpRequests(auth -> auth
-//                        .anyRequest().permitAll()
-//                )
-//                .build();
-//    }
-//}
+@Configuration
+public class SecurityConfig  {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        System.out.println("setting up bean for security config");
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                )
+                .build();
+    }
+
+    /**
+     * Ignores all requests for security configuration.
+     * This is not recommended for production.
+     * @return a {@link WebSecurityCustomizer} that ignores all requests
+     */
+    @Bean
+    WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().anyRequest();
+    }
+}
