329ee5107cdbd8826e6607c68bbf14e3c8be70fe
Mohammed Imthathullah <imthath.m@hotmail.com>
2025-02-11 15:36:36 +0530

    court service: Enhance court service model with additional fields and implement court retrieval method

--- court-service/src/main/java/com/imthath/food_street/court_service/Court.java
+++ court-service/src/main/java/com/imthath/food_street/court_service/Court.java
@@ -1,12 +1,10 @@
 package com.imthath.food_street.court_service;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+
+import java.time.Instant;
 
 @Entity
 @Table(name = "food_courts")
@@ -17,4 +15,18 @@ public class Court {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, length = 500)
+    private String location;
+
+    @Column(name = "image_url", length = 500)
+    private String imageUrl;
+
+    @Column(name = "created_at", updatable = false)
+    private Instant createdAt;
+
+    @Column(name = "updated_at")
+    private Instant updatedAt;
 }
--- court-service/src/main/java/com/imthath/food_street/court_service/CourtService.java
+++ court-service/src/main/java/com/imthath/food_street/court_service/CourtService.java
@@ -1,8 +1,16 @@
 package com.imthath.food_street.court_service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CourtService {
-    
+    @Autowired
+    CourtRepository courtRepository;
+
+    Optional<Court> getCourt(Long id) {
+        return courtRepository.findById(id);
+    }
 }
