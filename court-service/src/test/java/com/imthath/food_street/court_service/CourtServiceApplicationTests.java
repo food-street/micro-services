43fb2ff710852186ed19fcaaba12f6ae047aa4ed
Mohammed Imthathullah <imthath.m@hotmail.com>
2025-02-13 09:52:56 +0530

    court-service: Add integration tests with RestAssured and Testcontainers
    Introduced RestAssured for HTTP-based integration testing and configured Testcontainers with MySQL for isolated database tests. Added CRUD operation tests for the Court API, ensuring complete functionality coverage.

--- court-service/src/test/java/com/imthath/food_street/court_service/CourtServiceApplicationTests.java
+++ court-service/src/test/java/com/imthath/food_street/court_service/CourtServiceApplicationTests.java
@@ -1,15 +1,112 @@
 package com.imthath.food_street.court_service;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.utility.DockerImageName;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CourtServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@SuppressWarnings("resource")
+	@ServiceConnection
+	static MySQLContainer<?> mysql = new MySQLContainer<>(DockerImageName.parse("mysql:8.0.26"));
+
+	@LocalServerPort
+	private int port;
+
+	@BeforeEach
+	void setUp() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
 	}
 
+	@Test
+	void createCourt() {
+		given()
+				.contentType("application/json")
+				.body("{\"name\": \"Test Court\", \"location\": \"Test Location\", \"imageUrl\": \"http://example.com/image.jpg\"}")
+				.post("/court")
+				.then()
+				.statusCode(201)
+				.body("id", notNullValue())
+				.body("name", equalTo("Test Court"))
+				.body("location", equalTo("Test Location"))
+				.body("imageUrl", equalTo("http://example.com/image.jpg"));
+	}
+
+	@Test
+	void getAllCourts() {
+		createCourt();
+		createCourt();
+		given()
+				.get("/court")
+				.then()
+				.statusCode(200)
+				.body("size()", greaterThanOrEqualTo(2));
+	}
+
+	@Test
+	void getCourtById() {
+		var courtId = createSampleCourt();
+
+		given()
+				.get("/court/{id}", courtId)
+				.then()
+				.statusCode(200)
+				.body("id", equalTo(courtId))
+				.body("name", equalTo("Test Court"))
+				.body("location", equalTo("Test Location"))
+				.body("imageUrl", equalTo("http://example.com/image.jpg"));
+	}
+
+	@Test
+	void updateCourt() {
+		var courtId = createSampleCourt();
+
+		given()
+				.contentType("application/json")
+				.body("{\"name\": \"Updated Court\", \"location\": \"Updated Location\", \"imageUrl\": \"http://example.com/updated_image.jpg\"}")
+				.put("/court/{id}", courtId)
+				.then()
+				.statusCode(200)
+				.body("id", equalTo(courtId))
+				.body("name", equalTo("Updated Court"))
+				.body("location", equalTo("Updated Location"))
+				.body("imageUrl", equalTo("http://example.com/updated_image.jpg"));
+	}
+
+	@Test
+	void deleteCourt() {
+		var courtId = createSampleCourt();
+
+		given()
+				.delete("/court/{id}", courtId)
+				.then()
+				.statusCode(204);
+
+		given()
+				.get("/court/{id}", courtId)
+				.then()
+				.statusCode(901);
+	}
+
+	private Integer createSampleCourt() {
+		return given()
+				.contentType("application/json")
+				.body("{\"name\": \"Test Court\", \"location\": \"Test Location\", \"imageUrl\": \"http://example.com/image.jpg\"}")
+				.post("/court")
+				.then()
+				.statusCode(201)
+				.extract()
+				.path("id");
+	}
 }
