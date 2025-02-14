4280a7be9c2c1344c874f18d37afb82cee7adfb7
Mohammed Imthathullah <imthath.m@hotmail.com>
2025-02-14 09:03:40 +0530

    user-service: Add global exception handler bean
    Integrated a GlobalExceptionHandler bean to centralize error handling across the service. This enhances maintainability by leveraging a shared utility for consistent exception management.

--- user-service/src/main/java/com/imthath/food_street/user_service/UserServiceApplication.java
+++ user-service/src/main/java/com/imthath/food_street/user_service/UserServiceApplication.java
@@ -1,7 +1,9 @@
 package com.imthath.food_street.user_service;
 
+import com.imthath.utils.guardrail.GlobalExceptionHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class UserServiceApplication {
@@ -10,4 +12,8 @@ public class UserServiceApplication {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
+	@Bean
+	GlobalExceptionHandler exceptionHandler() {
+		return new GlobalExceptionHandler();
+	}
 }
