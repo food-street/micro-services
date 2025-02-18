9222594fd0ea4dd5ea9bc5294af7b684e3fb26a8
Mohammed Imthathullah <imthath.m@hotmail.com>
2025-02-18 21:29:37 +0530

    image-service: Update StorageConfig to use service account credentials from a specified location

--- image-service/src/main/java/com/imthath/food_street/image_service/config/StorageConfig.java
+++ image-service/src/main/java/com/imthath/food_street/image_service/config/StorageConfig.java
@@ -1,11 +1,15 @@
 package com.imthath.food_street.image_service.config;
 
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.auth.oauth2.ServiceAccountCredentials;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 
 import java.io.IOException;
 
@@ -13,15 +17,23 @@ import java.io.IOException;
 @EnableConfigurationProperties(StorageProperties.class)
 public class StorageConfig {
     
+    @Value("${google.cloud.credentials.location:classpath:service-account-key.json}")
+    private String credentialsLocation;
+    
+    private final ResourceLoader resourceLoader;
+    
+    public StorageConfig(ResourceLoader resourceLoader) {
+        this.resourceLoader = resourceLoader;
+    }
+    
     @Bean
     public Storage storage() throws IOException {
-        GoogleCredentials credentials = GoogleCredentials.getApplicationDefault();
-        if (credentials != null) {
-            System.out.println("Objectained credentials for project "  + credentials.getQuotaProjectId());
-        }
+        Resource credentialsResource = resourceLoader.getResource(credentialsLocation);
+        GoogleCredentials credentials = ServiceAccountCredentials.fromStream(credentialsResource.getInputStream());
+        
         return StorageOptions.newBuilder()
             .setCredentials(credentials)
             .build()
             .getService();
     }
-} 
+}
