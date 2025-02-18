fe516dfb49f72b4cab43818dfe908db87433695c
Mohammed Imthathullah <imthath.m@hotmail.com>
2025-02-18 15:10:09 +0530

    image-service: Configure GCP application default credentials for storage service
    - Update StorageConfig to use GoogleCredentials for GCP storage
    - Add credentials logging to verify project credentials
    - Modify storage bean to explicitly set credentials when initializing storage

--- image-service/src/main/java/com/imthath/food_street/image_service/config/StorageConfig.java
+++ image-service/src/main/java/com/imthath/food_street/image_service/config/StorageConfig.java
@@ -1,17 +1,27 @@
 package com.imthath.food_street.image_service.config;
 
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.IOException;
+
 @Configuration
 @EnableConfigurationProperties(StorageProperties.class)
 public class StorageConfig {
     
     @Bean
-    public Storage storage() {
-        return StorageOptions.getDefaultInstance().getService();
+    public Storage storage() throws IOException {
+        GoogleCredentials credentials = GoogleCredentials.getApplicationDefault();
+        if (credentials != null) {
+            System.out.println("Objectained credentials for project "  + credentials.getQuotaProjectId());
+        }
+        return StorageOptions.newBuilder()
+            .setCredentials(credentials)
+            .build()
+            .getService();
     }
 } 
