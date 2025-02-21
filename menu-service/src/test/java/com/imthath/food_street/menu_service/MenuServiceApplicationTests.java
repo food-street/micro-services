759e286183adb8a9aa67824d3f1e7627b29957cd
Mohammed Imthathullah <imthath.m@hotmail.com>
2025-02-21 07:26:39 +0530

    menu service: spring init

---
+++ menu-service/src/test/java/com/imthath/food_street/menu_service/MenuServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.imthath.food_street.menu_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class MenuServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
