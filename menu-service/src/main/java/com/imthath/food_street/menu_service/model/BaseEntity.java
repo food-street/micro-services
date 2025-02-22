0be1991781ec6f5aa1d44a90557ec8f81cd97fcb
Mohammed Imthathullah <imthath.m@hotmail.com>
2025-02-22 08:13:08 +0530

    menu: fix mongo db issues

--- menu-service/src/main/java/com/imthath/food_street/menu_service/model/BaseEntity.java
+++ menu-service/src/main/java/com/imthath/food_street/menu_service/model/BaseEntity.java
@@ -2,6 +2,7 @@ package com.imthath.food_street.menu_service.model;
 
 import jakarta.persistence.MappedSuperclass;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -13,6 +14,7 @@ import java.util.Date;
 @SuperBuilder
 @Data
 @MappedSuperclass
+@NoArgsConstructor
 public abstract class BaseEntity {
     @Id
     private String id;
--- menu-service/src/main/java/com/imthath/food_street/menu_service/model/Category.java
+++ menu-service/src/main/java/com/imthath/food_street/menu_service/model/Category.java
@@ -6,6 +6,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @SuperBuilder
 @Document(collection = "categories")
+@NoArgsConstructor
 public class Category extends BaseEntity {
 }
 
