6b6030058a392785320ab5d719b1714c800e8d2f
Imthath <imthath.m@hotmail.com>
2025-04-14 12:58:58 +0530

    get tools from all controllers via app context

--- menu-service/src/main/java/com/imthath/food_street/menu_service/MenuServiceApplication.java
+++ menu-service/src/main/java/com/imthath/food_street/menu_service/MenuServiceApplication.java
@@ -7,6 +7,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.ApplicationContext;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.ScannedGenericBeanDefinition;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.context.annotation.ComponentScan;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 @EnableMongoAuditing
@@ -20,7 +29,30 @@ public class MenuServiceApplication {
 	public GlobalExceptionHandler globalExceptionHandler() { return new GlobalExceptionHandler(); }
 
 	@Bean
-	public ToolCallbackProvider menuTools(MenuController menuController) {
-		return MethodToolCallbackProvider.builder().toolObjects(menuController).build();
+	public ToolCallbackProvider menuTools(ApplicationContext applicationContext) {
+		// Get all bean names
+		String[] beanNames = applicationContext.getBeanDefinitionNames();
+		List<Object> controllers = new ArrayList<>();
+		
+		// Filter for RestController annotated beans
+		for (String beanName : beanNames) {
+			// Skip the menuTools bean itself to avoid circular dependency
+			if ("menuTools".equals(beanName)) {
+				continue;
+			}
+			
+			try {
+				Object bean = applicationContext.getBean(beanName);
+				if (bean.getClass().isAnnotationPresent(RestController.class)) {
+					controllers.add(bean);
+				}
+			} catch (Exception e) {
+				// Skip if there's an error getting the bean
+			}
+		}
+		
+		return MethodToolCallbackProvider.builder()
+			.toolObjects(controllers.toArray())
+			.build();
 	}
 }
