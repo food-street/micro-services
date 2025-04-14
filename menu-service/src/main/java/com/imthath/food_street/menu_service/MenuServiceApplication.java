dacf3bfb08a15b3fd219872f1b7337078fd32a43
Imthath <imthath.m@hotmail.com>
2025-04-14 14:11:07 +0530

    try print request methods

--- menu-service/src/main/java/com/imthath/food_street/menu_service/MenuServiceApplication.java
+++ menu-service/src/main/java/com/imthath/food_street/menu_service/MenuServiceApplication.java
@@ -61,5 +61,66 @@ public class MenuServiceApplication {
 		return controllers;
 	}
 
-	
+	private void printRequestMappingInfo(ApplicationContext context) {
+		System.out.println("\n=== Request Mapping Information ===");
+		List<Object> controllers = getRestControllerBeans(context);
+		for (Object bean : controllers) {
+			Class<?> beanClass = bean.getClass();
+			System.out.println("\nController: " + beanClass.getSimpleName());
+			// Get class-level RequestMapping
+			RequestMapping classMapping = beanClass.getAnnotation(RequestMapping.class);
+			String basePath = "";
+			if (classMapping != null && classMapping.value().length > 0) {
+				basePath = classMapping.value()[0];
+			}
+			// Get all methods with RequestMapping annotations
+			for (Method method : beanClass.getDeclaredMethods()) {
+				String path = basePath;
+				String httpMethod = "UNKNOWN";
+				if (method.isAnnotationPresent(RequestMapping.class)) {
+					RequestMapping mapping = method.getAnnotation(RequestMapping.class);
+					if (mapping.value().length > 0) {
+						path += mapping.value()[0];
+					}
+					if (mapping.method().length > 0) {
+						httpMethod = mapping.method()[0].name();
+					}
+				} else if (method.isAnnotationPresent(GetMapping.class)) {
+					GetMapping mapping = method.getAnnotation(GetMapping.class);
+					if (mapping.value().length > 0) {
+						path += mapping.value()[0];
+					}
+					httpMethod = "GET";
+				} else if (method.isAnnotationPresent(PostMapping.class)) {
+					PostMapping mapping = method.getAnnotation(PostMapping.class);
+					if (mapping.value().length > 0) {
+						path += mapping.value()[0];
+					}
+					httpMethod = "POST";
+				} else if (method.isAnnotationPresent(PutMapping.class)) {
+					PutMapping mapping = method.getAnnotation(PutMapping.class);
+					if (mapping.value().length > 0) {
+						path += mapping.value()[0];
+					}
+					httpMethod = "PUT";
+				} else if (method.isAnnotationPresent(DeleteMapping.class)) {
+					DeleteMapping mapping = method.getAnnotation(DeleteMapping.class);
+					if (mapping.value().length > 0) {
+						path += mapping.value()[0];
+					}
+					httpMethod = "DELETE";
+				} else if (method.isAnnotationPresent(PatchMapping.class)) {
+					PatchMapping mapping = method.getAnnotation(PatchMapping.class);
+					if (mapping.value().length > 0) {
+						path += mapping.value()[0];
+					}
+					httpMethod = "PATCH";
+				}
+				if (!httpMethod.equals("UNKNOWN")) {
+					System.out.printf("  %-7s %s%n", httpMethod, path);
+				}
+			}
+		}
+		System.out.println("\n================================");
+	}
 }
